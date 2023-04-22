ec1ccd930d2075f37eda34fb22219e1810016fc0
MasterHard228 <118759082+MasterHard228@users.noreply.github.com>
2023-04-22 13:15:25 +0300

    commit8

---
+++ Image Mars/image_mars.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def mission():
+    return 'Миссия Колонизация Марса'
+
+
+@app.route('/index')
+def motto():
+    return 'И на Марсе будут яблони цвести!'
+
+
+@app.route('/promotion')
+def advertising():
+    return '''Человечество вырастает из детства.\n
+              Человечеству мала одна планета.\n
+              Мы сделаем обитаемыми безжизненные пока планеты.\n
+              И начнем с Марса!\n
+              Присоединяйся!'''
+
+
+@app.route('/image_mars')
+def image_mars():
+    return render_template('image_mars.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- Готовимся к миссии/app.py
+++ Готовимся к миссии/app.py
@@ -10,7 +10,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-a = 0
-if a == 0:
-    pass
