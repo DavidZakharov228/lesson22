9abd39c9424bbb045f3957c2d45eee0ab34f0263
MasterHard228 <118759082+MasterHard228@users.noreply.github.com>
2023-04-29 14:28:04 +0300

    commit26

---
+++ Тренировки в полёте/mars_flask.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/training/врач')
+def doctor():
+    return render_template('home.html')
+
+
+@app.route('/training/инженер')
+def engineer():
+    return render_template('home.html')
+
+
+@app.route('/training/строитель')
+def builder():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
